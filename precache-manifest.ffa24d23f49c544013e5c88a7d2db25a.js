dc2fd2ab76ffe865395b189027688c0174c57796
LAPTOP-J5SPII67\Acer <vikrantrath4@gmail.com>
2019-10-14 00:18:23 +0530

    Updates

---
+++ precache-manifest.ffa24d23f49c544013e5c88a7d2db25a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d76a73eeb7582d46be9b880b80ae8dad",
+    "url": "/VBuilds/index.html"
+  },
+  {
+    "revision": "e1cf34799623f256e64c",
+    "url": "/VBuilds/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "eab268312afeab8785bd",
+    "url": "/VBuilds/static/css/main.02fd2641.chunk.css"
+  },
+  {
+    "revision": "e1cf34799623f256e64c",
+    "url": "/VBuilds/static/js/2.a8362449.chunk.js"
+  },
+  {
+    "revision": "eab268312afeab8785bd",
+    "url": "/VBuilds/static/js/main.0457089f.chunk.js"
+  },
+  {
+    "revision": "5941c4caf97ee284e7df",
+    "url": "/VBuilds/static/js/runtime-main.154edd40.js"
+  }
+]);
