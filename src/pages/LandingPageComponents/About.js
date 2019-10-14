2b87c7690465fd613a8160e37ea3656cc0a68c7f
LAPTOP-J5SPII67\Acer <vikrantrath4@gmail.com>
2019-10-14 11:56:00 +0530

    Added about us design

--- src/pages/LandingPageComponents/About.js
+++ src/pages/LandingPageComponents/About.js
@@ -3,8 +3,20 @@ import './About.css'
 const About = ()=>{
     return <div className="item-b">
             <div className="first">
-                <h1 className="text-center">About US</h1>
-                <p>Under Construction</p>
+            <section class="section first-section">
+                <h1 style={{color:'black'}}>About Us</h1>
+    <p>
+       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris et velit magna. Donec lacus urna, egestas sed bibendum at, pellentesque ut tortor. Donec tempus neque nisl, quis gravida lectus bibendum sed.
+    </p>
+    <div class="diagonal-background">
+    </div>
+</section>
+<section class="section second-section">
+    <h1 style={{color:'white'}}>Our Mission</h1>
+    <p>
+        Morbi ac tristique magna, in cursus diam. Sed auctor laoreet dolor, at iaculis nulla accumsan eget. Fusce vitae diam malesuada metus eleifend euismod et quis urna. Nulla facilisi. In libero eros, aliquet quis facilisis posuere, rhoncus at ante.
+    </p>
+</section>
             </div>
         </div>
 }
